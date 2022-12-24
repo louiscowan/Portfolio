4557eab1b203865f519d3301fd066b93225f9563
Louis Cowan <louiscowan7@gmail.com>
2022-12-24 19:35:51 +1300

    yep

--- client/components/Home/index.jsx
+++ client/components/Home/index.jsx
@@ -21,6 +21,11 @@ function index () {
                         <Project h2={'Pokemon Finder'} p={'Search up your facourite Pokemon, look at the Pokemon\'s stats, and add them to the data base'}/>
                     </a>
                 </div>
+                <div>
+                    <a href="https://at-what-cost.devacademy.nz">
+                        <Project h2={'Financial Spending Tracker'} p={'This is my final group project from Dev Academy. This app allows you to upload a csv file and look at what you have spent your money on. You are also able to put filters on different purchases to be able to see a graph breakdown of your spending habbits. Login and your filters will be saved.'}/>
+                    </a>
+                </div>
             </div>
         </>
     )
--- client/components/Navbar/index.jsx
+++ client/components/Navbar/index.jsx
@@ -6,13 +6,18 @@ function Index () {
     return (
         <div className="navbar">
             <div className="navbarStuff">
-                <ul>
-                    <li>
-                        <a href="https://github.com/louiscowan" target="_blank">
-                            <img src="https://github.githubassets.com/images/modules/logos_page/Octocat.png" alt="github logo"/>
-                        </a>
-                    </li>
-                </ul>
+                <div>
+                    <ul>
+                        <li>
+                            <h2>Louis Portfolio</h2>
+                        </li>
+                    </ul>
+                </div>
+                <div>
+                    <a href="https://github.com/louiscowan" target="_blank">
+                        <img src="https://github.githubassets.com/images/modules/logos_page/Octocat.png" alt="github logo"/>
+                    </a>
+                </div>
             </div>
         </div>
     )
